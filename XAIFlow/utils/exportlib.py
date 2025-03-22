e915ab25dae3268824dff18f9973b5d20ca29bc0
JNeubau <neubauer.asia@gmail.com>
2025-03-22 20:58:17 +0100

    replaced function for saving excel with similar containing sorting

--- XAIFlow/utils/exportlib.py
+++ XAIFlow/utils/exportlib.py
@@ -44,7 +44,7 @@ def save_data_to_excel(data, smiles_list, excel_file):
     clean_up_png_files_from_dir("png")
     
 
-def save_data_to_excel_with_highlights(data, smiles_list, smarts_list, excel_file):
+def save_data_to_excel_with_highlights_no_sort(data, smiles_list, smarts_list, excel_file):
     """
     Save data to an Excel file with molecule images generated from SMILES strings and highlighted substructures.
 
@@ -63,6 +63,45 @@ def save_data_to_excel_with_highlights(data, smiles_list, smarts_list, excel_fil
         worksheet = writer.sheets["Data"]
 
         for i, (smiles, smarts) in enumerate(zip(smiles_list, smarts_list)):
+            mol = Chem.MolFromSmiles(smiles)
+            match_smart = Chem.MolFromSmarts(smarts)
+            if mol and match_smart:
+                os.makedirs(image_dir, exist_ok=True)
+                img_path = image_dir + f"\\mol_{i}.png"
+                highlight_atoms = [atom for match in mol.GetSubstructMatches(match_smart) for atom in match]
+                Draw.MolToFile(mol, img_path, highlightAtoms=highlight_atoms)
+
+                row = i + 1
+                col = 0
+                worksheet.insert_image(row, col, img_path)
+
+                worksheet.set_row(row, 250)
+    clean_up_png_files_from_dir(image_dir)
+    
+    
+def save_data_to_excel_with_highlights(data, smiles_list, smarts_list, excel_file):
+    """
+    Save data to an Excel file with molecule images generated from SMILES strings and highlighted substructures.
+    The data will be sorted by SMILES and a specified feature.
+
+    Parameters:
+    - data (dict): A dictionary containing the data to be saved.
+                   Keys should be column names, and values should be lists of column data.
+    - smiles_list (list): A list of SMILES strings corresponding to the molecules.
+    - smarts_list (list): A list of SMARTS patterns to highlight in the molecules.
+    - excel_file (str): The path to the Excel file where the data will be saved.
+    """
+    image_dir = os.path.dirname(os.getcwd()) + '\\png'
+    df = pd.DataFrame(data)
+
+    df['SMILES'] = smiles_list
+    df = df.sort_values(by=['SMILES', 'Feature'])
+
+    with pd.ExcelWriter(excel_file, engine="xlsxwriter") as writer:
+        df.to_excel(writer, index=False, sheet_name="Data", startrow=0, startcol=6)
+        worksheet = writer.sheets["Data"]
+
+        for i, (smiles, smarts) in enumerate(zip(df['SMILES'], smarts_list)):
             mol = Chem.MolFromSmiles(smiles)
             match_smart = Chem.MolFromSmarts(smarts)
             if mol and match_smart:
