d8427e6cb7b540a3ac201e20e4d97e7de7c5e198
JNeubau <neubauer.asia@gmail.com>
2025-05-28 16:02:46 +0200

    saving raw xlsx file and processing json, wip processing df

--- MEG/analysis/meg_excel_anal.py
+++ MEG/analysis/meg_excel_anal.py
@@ -12,15 +12,11 @@ def find_json_files(base_dir):
 
 def parse_json_file(json_file):
     folder_name = os.path.basename(os.path.dirname(json_file))
-    try:
-        fold_no, mol_id = folder_name.split("_", 1)
-    except ValueError:
-        fold_no, mol_id = None, None
+    fold_no, mol_id = folder_name.split("_", 1)
 
     with open(json_file, "r") as f:
         data = json.load(f)
 
-    print(data)
     rows = []
     if isinstance(data, dict):
         data_row = {"fold_no": fold_no, "mol_id": mol_id}
@@ -28,7 +24,6 @@ def parse_json_file(json_file):
         rows.append(data_row)
     elif isinstance(data, list):
         for entry in data:
-            # print('ent', entry)
             data_row = {"fold_no": fold_no, "mol_id": mol_id}
             if isinstance(entry, dict):
                 data_row.update(entry)
@@ -39,14 +34,46 @@ def parse_json_file(json_file):
 
 def collect_all_data(json_files):
     all_data = []
-    for json_file in json_files[:1]:
+    for json_file in json_files:
         all_data.extend(parse_json_file(json_file))
     return all_data
 
-def save_to_excel(data, output_excel):
-    df = pd.DataFrame(data)
-    df.to_excel(output_excel, index=False)
-    print(f"Data saved to {output_excel}")
+def convert_json_to_dataframel(data):
+    df = pd.DataFrame(data)    
+    if 'prediction' in df.columns:
+        if not df['prediction'].isna().all():
+            # Extract nested values
+            df['prediction_type'] = df['prediction'].apply(
+                lambda x: x.get('type') if isinstance(x, dict) else None)
+            df['prediction_output'] = df['prediction'].apply(
+                lambda x: x.get('output') if isinstance(x, dict) else None)
+            df['prediction_for_explanation'] = df['prediction'].apply(
+                lambda x: x.get('for_explanation') if isinstance(x, dict) else None)
+            df['prediction_class'] = df['prediction'].apply(
+                lambda x: x.get('class') if isinstance(x, dict) else None)
+            df['prediction_difference'] = df['prediction'].apply(
+                lambda x: x.get('difference') if isinstance(x, dict) else None)
+            df['prediction_original'] = df['prediction'].apply(
+                lambda x: x.get('original') if isinstance(x, dict) else None)
+            df.drop(['prediction'], axis=1, inplace=True)
+    
+    original_df = df[df['marker'] == 'og'].copy()
+    original_df.drop(['reward', 'reward_pred', 'reward_sim', 'features', 'id',
+                      'prediction_difference', 'prediction_original'], 
+                    axis=1, inplace=True)
+    counterfactual_df = df[df['marker'] == 'cf'].copy()
+    counterfactual_df.drop(['prediction_for_explanation', 'prediction_class'], 
+                    axis=1, inplace=True)
+    return original_df, counterfactual_df
+
+def process_dataframes(og_df, cf_df):
+    result_df = pd.DataFrame(columns=['fold_no', 'mol_id', 'og_prediction_type'])
+    return result_df
+
+def save_to_excel(df: pd.DataFrame, output_dir, output_excel):
+    df.to_excel(os.path.join(output_dir, output_excel), index=False)
+    print(f"Data successfully saved to {os.path.join(output_dir, output_excel)}")
+    return df
 
 def anal_global():
     experiment_name = "new_test" 
@@ -55,8 +82,16 @@ def anal_global():
 
     json_files = find_json_files(workdir)
     all_data = collect_all_data(json_files)
-    print(all_data[:3], len(all_data), all_data[0].keys()) 
-    # save_to_excel(all_data, output_excel)
+    og_df, cf_df = convert_json_to_dataframel(all_data)
+    print(og_df.T)
+    print(cf_df.T)
+    save_to_excel(og_df, os.path.join(workdir, 'global'), 'raw_og_' + output_excel)
+    save_to_excel(cf_df, os.path.join(workdir, 'global'), 'raw_cf_' + output_excel)
+    
+    result_df = process_dataframes(og_df, cf_df)
+    print(result_df)
+    print(result_df.T)
+    # save_to_excel(result_df, os.path.join(workdir, 'global'), output_excel)
 
 if __name__ == "__main__":
     anal_global()
